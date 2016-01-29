7c6cacdc34129bdb323a27c02d259b93cd715bf5
dourgulf <dourgulf@gmail.com>
2016-01-29 15:44:11 +0800

    Change router to /

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+/**
+ * Created by Darwin Rie on 16/1/14.
+ */
+
 var express = require('express');
 var router = express.Router();
+var debug = require('debug')('myapi:qrcode');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var qr = require('qr-image');
+
+router.get('/qrcode', function (req, res) {
+  var text = req.query.text;
+  var size = (req.query.size || 10) * 1;    // 强制转成数字
+
+  debug("text:" + text);
+  debug("size:" + size);
+  try {
+    var img = qr.image(text,{size :size, type: "png"});
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    img.pipe(res);
+  } catch (e) {
+    res.writeHead(414, {'Content-Type': 'text/html'});
+    res.end('<h1>414 Request-URI Too Large</h1>');
+  }
 });
 
 module.exports = router;
